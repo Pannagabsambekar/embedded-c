90b1cb81aaa4ea6e8d5b0ebdeb57a648b9a12b4f
Pannagabsambekar <pannagabs2025@gmail.com>
2026-09-19 15:29:09 +0530

    Logical AND

---
+++ Bitandlogicaland.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int age =34;
+    char name[] = "Kempa";
+    if(age >24&& strcmp(name , "Kempa")==0){
+        printf("He is eligible");}
+     else{
+        printf("He is not eligible");
+    }
+
+
+}
--- bitwiseNOT.c
+++ bitwiseNOT.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main(){
     int x =60;
-    
-    printf("output = %d",~x);
+     printf("output = %d",~x);
     return 0;
 }
